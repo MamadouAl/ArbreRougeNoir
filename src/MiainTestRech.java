7a6f3de508213c08ac97e596e0f27d6f0d769e45
MamadouAl <tygaaliou@gmail.com>
2023-12-02 03:39:19 +0100

    Mis à jour des tests d'ajout et de recherche avec calcul de performances

--- src/MiainTestRech.java
+++ src/MiainTestRech.java
@@ -1,20 +1,22 @@
 import java.util.Random;
 
-public class MainTest {
+public class MiainTestRech {
     public static void main(String[] args) {
-        // Taille_max des arbres
-        int taille = 100_000;
-        int incremente = 10_000;
+        //Taille des arbres
+        int taille = 70000;
+        int incremente = 7000;
+        ABR<Integer> abr = new ABR<>();
+        ARN<Integer> arn = new ARN<>();
+
         //stocke les valeurs à chercher
         int[] stockeValeur = new int[taille];
 
         // Test du cas moyen où les clefs sont ajoutées en ordre aléatoire
-        System.out.println("***** Cas moyen : Ajout de clefs aléatoires *************");
+        System.out.println("***** Recherche de clefs aléatoires *************");
         System.out.printf("%12s%12s%12s%n", "TAILLE", "ABR_1(ms)", "ARN_1(ms)");
         // Construction de l'ABR et l'ARN
-        ABR<Integer> abr1 = new ABR<>();
-        ARN<Integer> arn1 = new ARN<>();
         Random random = new Random();
+
 
         for (int i = incremente; i < taille; i += incremente) {
 
@@ -23,51 +25,53 @@ public class MainTest {
             }
 
             //Ajout dans ABR
+            for (int indice = 0; indice < i; indice++) {
+                abr.add(stockeValeur[indice]);
+                 arn.add(stockeValeur[indice]);
+            }
+
+            //Recherche dans ABR
             long t0 = System.currentTimeMillis();
             for (int indice = 0; indice < i; indice++) {
-                abr1.add(stockeValeur[indice]);
+                abr.contains(stockeValeur[indice]);
             }
             long t1 = System.currentTimeMillis() - t0;
 
-            //Ajout dans ARN
+            //Recherche dans ARN
             long t2 = System.currentTimeMillis();
             for (int indice = 0; indice < i; indice++) {
-                arn1.add(stockeValeur[indice]);
+                arn.contains(stockeValeur[indice]);
             }
             long t3 = System.currentTimeMillis() - t2;
 
             System.out.printf("%12d%12d%12d%n", i, t1, t3);
         }
+        
 
-
-        // Test du cas moyen où les clefs sont ajoutées en ordre aléatoire
-        System.out.println("\n***** Cas Defavorable : Ajout de clefs dans l'ordre croissant *************");
-        System.out.printf("%12s%12s%12s%n", "TAILLE", "ABR_2(ms)", "ARN_2(ms)");
-        // Construction de l'ABR et l'ARN
         ABR<Integer> abr2 = new ABR<>();
         ARN<Integer> arn2 = new ARN<>();
 
-        for (int indice = 0; indice < taille; indice += incremente) {
-            for (int k = 0; k < indice; k++) {
-                stockeValeur[k] = k;
-            }
 
-            //Ajout dans ABR
+        System.out.println("***** Recherche de clefs croissantes *************");
+        System.out.printf("%12s%12s%12s%n", "TAILLE", "ABR_2(ms)", "ARN_2(ms)");
+        for(int indice = incremente; indice < taille; indice += incremente) {
+            for (int i = 0; i < indice; i++) {
+                abr2.add(i);
+                arn2.add(i);
+            }
             long t0 = System.currentTimeMillis();
-            for (int j = 0; j < indice; j++) {
-                abr2.add(stockeValeur[j]);
+            for (int i = 0; i <2* indice; i++) {
+                abr2.contains(i);
             }
             long t1 = System.currentTimeMillis() - t0;
 
-            //Ajout dans ARN
             long t2 = System.currentTimeMillis();
-            for (int j = 0; j < indice; j++) {
-                arn2.add(stockeValeur[j]);
+            for (int i = 0; i < 2*indice; i++) {
+                arn2.contains(i);
             }
             long t3 = System.currentTimeMillis() - t2;
             System.out.printf("%12d%12d%12d%n", indice, t1, t3);
         }
-
+        
     }
-
 }
